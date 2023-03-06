c34b70e7acf4023294df84eea7e7ac3f1f912fea
Tan Thuyen Nguyen <127068064+nt-thuyen@users.noreply.github.com>
2023-03-06 10:52:09 +0700

    Add files via upload

---
+++ HTdieukhiennhietdophong.m
@@ -0,0 +1,7 @@
+%% Khai bao thong so
+Kv=1; tv=3; % Van dien tu
+Kb=5; % Buong dot
+RT=0.1; CT=81.87;
+Kh=1; th=1; % Cam bien nhiet PT-100
+t_mt=13; % Nhiet do moi truong
+tbd_phong=15; % Nhiet do phong ban dau
